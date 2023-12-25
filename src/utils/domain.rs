60a9ac50427b6514e55d0f91fe8784eaffde4c73
ludyna <sikachyna.n@gmail.com>
2023-12-25 16:04:24 +0200

    Remake domain validation

--- src/utils/domain.rs
+++ src/utils/domain.rs
@@ -1,6 +1,4 @@
-pub fn validate_domain(domain_str: &str) -> bool {
-    match url::Url::parse(domain_str) {
-        Ok(_) => true,
-        Err(_) => false
-    }
+pub fn validate_domain(domain: &str) -> bool {
+    let regex = regex::Regex::new(r"^(http|https)://(www\.)?[a-z0-9]+(\.[a-z]+)+(:[0-9]+)?(/.*)?$").unwrap();
+    regex.is_match(domain)
 }
